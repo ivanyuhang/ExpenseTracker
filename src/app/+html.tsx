e7213eae550572a1503dfb445c212cd1144f5305
Ivan Weng <ivan.yuhang.weng@gmail.com>
2026-09-23 10:30:00 +0200

    Enable iPhone standalone full-screen mode

---
+++ src/app/+html.tsx
@@ -0,0 +1,32 @@
+import { ScrollViewStyleReset, useServerDocumentContext } from 'expo-router/html';
+import type { ReactNode } from 'react';
+
+export default function RootHtml({ children }: { children: ReactNode }) {
+  const { bodyAttributes, bodyNodes, htmlAttributes, headNodes } = useServerDocumentContext();
+
+  return (
+    <html lang="en" {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#208AEF" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Pocket Plan" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="format-detection" content="telephone=no" />
+        <title>Pocket Plan</title>
+        <ScrollViewStyleReset />
+        {headNodes}
+      </head>
+      <body {...bodyAttributes}>
+        {children}
+        {bodyNodes}
+      </body>
+    </html>
+  );
+}
